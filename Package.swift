74d7176a155057f7009e5f2dadfc54cbd53a7ba9
Doug <dougstein@gmail.com>
2018-07-25 15:36:53 -0700

    Travis: fix target for CPKAlamofireCT build
    SPM: add proper PMKAlamofire dependency to CPKAlamofire config file
    
    SPM: add proper PMKFoundation dependency to CPKFoundation config file
    
    SPM: add proper PMKCoreLocation dependency to CPKCoreLocation config file
    
    Travis: temporarily disable Xcode 8.3 tests for CPKAlamofire and CPKFoundation
    
    Travis: temporarily disable iOS 8.4 and tvOS 9.2 tests for CancelForPromiseKit
    
    Travis: temporarily remove '-quiet' flag to diagnose build failures for CPKFoundation

--- Package.swift
+++ Package.swift
@@ -3,6 +3,7 @@ import PackageDescription
 let package = Package(
     name: "CPKCoreLocation",
     dependencies: [
+        .Package(url: "https://github.com/PromiseKit/CoreLocation", majorVersion: 3),
         .Package(url: "https://github.com/dougzilla32/CancelForPromiseKit", majorVersion: 1)
     ],
     exclude: [
